bb2ecc4ce14f211706148889083d0f5d402a3f76
Joshua Bello <joshuabello2550@gmail.com>
2023-06-29 04:28:44 +0000

    stuff

--- code.py
+++ code.py
@@ -1,47 +1,74 @@
-import tweepy
+from requests_oauthlib import OAuth1Session
 import os
-import pandas as pd
-from collections import Counter
+import json
 
-def authenticate_twitter_api(consumer_key, consumer_secret, access_token, access_token_secret):
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
+# In your terminal please set your environment variables by running the following lines of code.
+# export 'CONSUMER_KEY'='<your_consumer_key>'
+# export 'CONSUMER_SECRET'='<your_consumer_secret>'
 
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-    return api
+consumer_key = os.getenv('TWITTER_CONSUMER_KEY')
+consumer_secret = os.getenv('TWITTER_CONSUMER_SECRET')
+print(consumer_key)
+print(consumer_secret)
 
-def get_user_timeline(api, screen_name):
-    tweets = api.user_timeline(screen_name=screen_name, count=20)
-    data=[]
-    for tweet in tweets:
-        hashtags = [h['text'] for h in tweeet.entities['hashtags']]
-        data.append({
-            'id': tweet.id_str,
-            'created_at': tweet.created_at,
-            'text': tweet.full_text,
-            'hashtags': hashtags,
-        })
-    return pd.DataFrame(data)
+# Get the number of miles ran from the user
+miles = input("Enter the number of miles you ran today: ")
 
-def get_trending_hashtags(df):
-    all_hashtags = [h for row in df['hashtags'] for h in row]
-    counter = Counter(all_hashtags)
-    return counter.most_common(10)
+payload = {"text": f"I ran {miles} miles today! #Running"}
 
-if __name__=="__main__":
-    consumer_key = os.getenv('TWITTER_CONSUMER_KEY')
-    consumer_secret = os.getenv('TWITTER_CONSUMER_SECRET')
-    access_token = os.getenv('TWITTER_ACCESS_TOKEN')
-    access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
-    api = authenticate_twitter_api(consumer_key, consumer_secret, access_token, access_token_secret)
-    print("testing")
-    user_tweets =  get_user_timeline(api, 'UWHS75819288')
-    
-    trends = get_trending_hashtags(user_tweets)
-    print("Top Trending hashtags")
-    for hashtag, count in trends:
-        print(f"{hashtag}: {count}")
+# Get request token
+request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
+oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
 
+try:
+    fetch_response = oauth.fetch_request_token(request_token_url)
+except ValueError:
+    print(
+        "There may have been an issue with the consumer_key or consumer_secret you entered."
+    )
 
+resource_owner_key = fetch_response.get("oauth_token")
+resource_owner_secret = fetch_response.get("oauth_token_secret")
+print("Got OAuth token: %s" % resource_owner_key)
+
+# Get authorization
+base_authorization_url = "https://api.twitter.com/oauth/authorize"
+authorization_url = oauth.authorization_url(base_authorization_url)
+print("Please go here and authorize: %s" % authorization_url)
+verifier = input("Paste the PIN here: ")
+
+# Get the access token
+access_token_url = "https://api.twitter.com/oauth/access_token"
+oauth = OAuth1Session(
+    consumer_key,
+    client_secret=consumer_secret,
+    resource_owner_key=resource_owner_key,
+    resource_owner_secret=resource_owner_secret,
+    verifier=verifier,
+)
+oauth_tokens = oauth.fetch_access_token(access_token_url)
+
+access_token = oauth_tokens["oauth_token"]
+access_token_secret = oauth_tokens["oauth_token_secret"]
+
+# Make the request
+oauth = OAuth1Session(
+    consumer_key,
+    client_secret=consumer_secret,
+    resource_owner_key=access_token,
+    resource_owner_secret=access_token_secret,
+)
+
+# Making the request
+response = oauth.post(
+    "https://api.twitter.com/2/tweets",
+    json=payload,
+)
+if response.status_code == 201:
+    print("Tweet posted successfully!")
+else:
+    print("Error posting tweet:", response.json())
+
+# Saving the response as JSON
+json_response = response.json()
+print(json.dumps(json_response, indent=4, sort_keys=True))
